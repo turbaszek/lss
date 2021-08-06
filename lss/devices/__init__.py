4e2aedb49497f998872e9b3df4b692fbb3e261a4
Isaiah Sugar <readingmaniac2004@gmail.com>
2021-08-06 11:08:00 -0400

    added launchpad x support

--- lss/devices/__init__.py
+++ lss/devices/__init__.py
@@ -1,5 +1,6 @@
 from lss.devices.launchpad_mini_3 import LaunchpadMiniMk3
+from lss.devices.launchpad_x import LaunchpadX
 
-DEVICES = {LaunchpadMiniMk3.name: LaunchpadMiniMk3}
+DEVICES = {LaunchpadMiniMk3.name: LaunchpadMiniMk3, LaunchpadX.name: LaunchpadX}
 
 DEVICES_NAMES = DEVICES.keys()
